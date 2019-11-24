dc24f5fd8ec35b88be90f4341cab38c6cbfae06c
Aashto <proy.aashto@gmail.com>
2019-11-23 23:35:23 -0500

    Update Asfalto

---
+++ SIS_Ga2/Controllers/ParametrosAdoquinController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIS_Ga2.Controllers
+{
+    public class ParametrosAdoquinController : Controller
+    {
+        // GET: ParametrosAdoquin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
